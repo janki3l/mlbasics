996e4770c8c0d715b7662c448bce02989d7cb1e7
janki3l <kontakt.jankowski.adam@gmail.com>
2023-03-06 13:02:00 +0100

    init upload v2

--- creditScore.py
+++ creditScore.py
@@ -7,13 +7,15 @@ import math
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 
-#zaczytaj dane z pliku csv
-df_train = pd.read_csv("/files/test.csv" , sep = "," , encoding
-= 'utf-8')
-#sprawdź liczbę kolumn i wierszy
-df.shape
-df.info()
-#wyświetl część tabeli
-df_train.head()
+
+df_train = pd.read_csv("./files/train.csv" , sep = "," , encoding = 'utf-8')
+
+#print(df_train.shape)
+#print(df_train.info())
+
 #usuń wiersze z duplikatami id
-df.drop_duplicates(subset="ID", inplace=True)
+
+df_train.drop_duplicates(subset="ID", inplace=True)
+
+
+print(df_train.describe())
